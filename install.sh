ddc4536f820c42524d81916a3d8fec0d890715c9
Iewnfod <shangzhiqiuyue@outlook.com>
2024-08-19 12:14:13 +0800

    fix: install script with region

--- install.sh
+++ install.sh
@@ -1,23 +1,60 @@
 #!/bin/bash
 
+region='auto'
+
+if [ -n "$1" ]; then
+	region="$1"
+fi
+
 runnable() {
 	type "$1" 1>/dev/null 2>/dev/null
 }
+
+is_chinese_ip() {
+    # 获取当前 IP 地址
+    local ip=$(curl -s ifconfig.me)
+
+    # 查询 IP 的地理位置
+    local location=$(curl -s "http://ip-api.com/json/${ip}?lang=zh-CN" | grep '"country":"中国"')
+
+    if [ -n "$location" ]; then
+        # echo "当前 IP ($ip) 是中国的。"
+        return 0
+    else
+        # echo "当前 IP ($ip) 不是中国的。"
+        return 1
+    fi
+}
+
+brew_remote='https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh'
+brew_tap='createchstudio/cpc'
+install_name='cpc'
+remote='github'
+
+if is_chinese_ip; then
+	if [ "$region" == "auto" ] || [ "$region" == "china" ]; then
+		brew_remote='https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh'
+		brew_tap='lightum_cc/cpc https://gitee.com/lightum_cc/homebrew-cpc.git'
+		install_name='cpc-cn'
+		remote='gitee'
+	fi
+fi
 
 if runnable brew; then
 	echo "Homebrew exists"
 else
 	echo "Try to install Homebrew for you now"
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	/bin/bash -c "$(curl -fsSL $brew_remote)"
 fi
 
 echo "Installing..."
 
-brew tap createchstudio/cpc
-brew install cpc
+brew tap $brew_tap
+brew install $install_name
 
 if runnable cpc; then
 	echo "Installing dependencies"
+	cpc -c remote $remote
 	cpc -i
 
 	echo "Install CAIE_Code successfully"
